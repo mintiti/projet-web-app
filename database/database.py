6d413a112f1a8f5cbd4cce264655b13c2ac06417
Minh Tri <minhtri3007@hotmail.com>
2020-03-11 12:05:00 +0100

    commit(db) decorator for lisibility

--- database/database.py
+++ database/database.py
@@ -1,8 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from contextlib import contextmanager
 db = SQLAlchemy()
 
 
 def init_database():
     db.create_all()
+
+
+def commit(db): #commit decorator
+    def decorated(func):
+        def f(*args,**kwargs):
+            func(*args,**kwargs)
+            db.session.commit()
+            return func(*args,**kwargs)
+        return f
+    return decorated
+
+
 
 
