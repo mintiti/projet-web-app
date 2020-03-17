e3ca7885f0bc9c4ecdfc89173c16244672a1bb61
Minh Tri <minhtri3007@hotmail.com>
2020-03-17 11:23:46 +0100

    now routes and renders the drinks,  desserts and sandwiches views correctly

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import flask
 from database.models import *
 from database.database_init import *
+
 app = flask.Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "secret_key1234"
 
-# @app.route("/clean")
+
 def clean():
     db.drop_all()
     db.create_all()
     initialize_ingredients()
+    initialize_desserts()
     initialize_drinks()
     initialize_sandwiches()
     initialize_users()
     initialize_menu_prices()
+
     return "Cleaned!"
+
 
 db.init_app(app)
 
@@ -23,11 +27,8 @@ with app.test_request_context():
     clean()
 
 
-
-
 @app.route("/test")
 def test():
-    clean()
 
     # # Create two sports
     # judo = Sport(name="judo")
@@ -70,6 +71,38 @@ def test():
     #
     # return flask.render_template("index.jinja2", sports=sports)
 
+    return flask.render_template("base.html.jinja2")
+
+
+@app.route("/")
+def home():
+    return flask.render_template("base.html.jinja2")
+
+
+@app.route("/sandwichs")
+def sandwichs():
+    sandwiches = Products.query.filter(Products.food_type == SANDWICH).all()
+    return flask.render_template("sandwiches.html.jinja2", products=sandwiches)
+
+
+@app.route("/boissons")
+def boissons():
+    drinks = Products.query.filter(Products.food_type == DRINK).all()
+    return flask.render_template("boissons.html.jinja2", products=drinks)
+
+
+@app.route("/menus")
+def menus():
+    pr = Products.query.filter(Products.id == 5).all()[0]
+    return flask.render_template("menus.html.jinja2", product=pr)
+
+
+@app.route("/desserts")
+def desserts():
+    des = Products.query.filter(Products.food_type == DESSERT).all()
+    return flask.render_template("desserts.html.jinja2", products = des)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
