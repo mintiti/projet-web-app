050e60e293b85d617d10fc33dc483435b3ea7ddf
Minh Tri <minhtri3007@hotmail.com>
2020-03-18 12:28:26 +0100

    Trying to get the form right

--- database/API.py
+++ database/API.py
@@ -13,9 +13,13 @@ def add_products(list_products):
         if product.food_type == SANDWICH:
             new_menu_price_entry(product)
 
-
+@commit(db)
 def new_menu_price_entry(sandwich):
     price = 5 if sandwich.price <= 2.1 else 7
     db.session.add(PrixMenu(sandwich_principal= sandwich.id, prix= price))
+
+
+def add_product_to_order():
+    pass
 
 #TODO : database accessors for sandwiches, drinks, desserts and menus
