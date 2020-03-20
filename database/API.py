1c1f488ed47a062534df029f49a9121fc027b90f
Minh Tri <minhtri3007@hotmail.com>
2020-03-20 17:47:22 +0100

    Added a stock checker wrapper, added an API entry point for menus

--- database/API.py
+++ database/API.py
@@ -24,5 +24,43 @@ def add_product_to_order(form):
     orderitem = OrderItem(order_id= 1, item=form['product_id'], quantity= form['quantity'])
     db.session.add(orderitem)
 
+#TODO : add order id tracking
+@commit(db)
+def add_menu_to_order(form):
+    for item in form:
+        o_item = OrderItem(order_id= 1, item = form[item], quantity= 1)
+        db.session.add(o_item)
 
-#TODO : database accessors for sandwiches, drinks, desserts and menus
+# Database accessors
+def check_stock(func):
+    """Function wrapper, returns a product list with only products not out of stock"""
+    def f(*args, **kwargs):
+        ret = func(*args,**kwargs)
+        for product in ret:
+            if product.stock == 0:
+                ret.remove(product)
+        return ret
+    return f
+
+
+def get_product_by_ID(product_id):
+    product = Products.query.filter(Products.id == product_id).all()[0]
+    return product
+
+def get_menu_prices():
+    menu_prices_list = PrixMenu.query.all()
+    sandwich_data = [get_product_by_ID(menu.sandwich_principal) for menu in menu_prices_list]
+    sandwich_dict ={}
+    for sand in sandwich_data:
+        sandwich_dict[sand.id] = sand.name
+    return menu_prices_list, sandwich_dict
+
+@check_stock
+def get_drinks():
+    return Products.query.filter(Products.food_type == DRINK).all()
+@check_stock
+def get_sandwiches():
+    return Products.query.filter(Products.food_type == SANDWICH).all()
+@check_stock
+def get_desserts():
+    return Products.query.filter(Products.food_type == DESSERT).all()
