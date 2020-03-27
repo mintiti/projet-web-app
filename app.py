e8dc39632fad68c02e1e12c706d1d570d87a88ad
Minh Tri <minhtri3007@hotmail.com>
2020-03-27 16:11:20 +0100

    Removed relics of old code

--- app.py
+++ app.py
@@ -30,52 +30,6 @@ with app.test_request_context():
     clean()
 
 
-@app.route("/test")
-def test():
-    # # Create two sports
-    # judo = Sport(name="judo")
-    # football = Sport(name="football")
-    # db.session.add(judo)
-    # db.session.add(football)
-    # db.session.commit()
-    #
-    # # Create three players
-    # player1 = Player(firstname="Jonathan", lastname="Pastor")
-    # player2 = Player(firstname="Hervé", lastname="Dupont")
-    # player3 = Player(firstname="André", lastname="Laroute")
-    # db.session.add(player1)
-    # db.session.add(player2)
-    # db.session.add(player3)
-    # db.session.commit()
-    #
-    # # Create a team with the two players
-    # rocket = Team(name="rocket", sport=judo)
-    # sonette = Team(name="sonette", sport=football)
-    # db.session.add(rocket)
-    # db.session.add(sonette)
-    # db.session.commit()
-    # #
-    # # # Add the two players to the team 'rocket'
-    # # rocket.players.append(player1)
-    # # rocket.players.append(player2)
-    # # db.session.add(rocket)
-    # # # Add the two players to the team 'sonette'
-    # # sonette.players.append(player2)
-    # # sonette.players.append(player3)
-    # # db.session.commit()
-    # # # Remove one player from the team 'sonette'
-    # # sonette.players.remove(player3)
-    # # db.session.add(sonette)
-    # # db.session.commit()
-    #
-    # # Fetch all sports
-    # sports = Sport.query.all()
-    #
-    # return flask.render_template("index.jinja2", sports=sports)
-
-    return flask.render_template("base.html.jinja2")
-
-
 @app.route("/")
 def home():
     return flask.render_template("base.html.jinja2")
@@ -107,7 +61,8 @@ def menus():
     if flask.request.method == 'POST':
         form = flask.request.form
         API.add_menu_to_order(form)
-    return flask.render_template("menus.html.jinja2", menus_list=menu_prices_list, sandwich_dict=sandwich_dict, drinks = drinks, des = des)
+    return flask.render_template("menus.html.jinja2", menus_list=menu_prices_list, sandwich_dict=sandwich_dict,
+                                 drinks=drinks, des=des)
 
 
 @app.route("/desserts", methods=['GET', "POST"])
@@ -117,8 +72,6 @@ def desserts():
         API.add_product_to_order(form)
     des = API.get_desserts()
     return flask.render_template("desserts.html.jinja2", products=des)
-
-
 
 
 if __name__ == '__main__':
